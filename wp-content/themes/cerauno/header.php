a0c67b2eddbf787fc4498a2f0acdf4ffd6ea01e4
long cheng <longcheng0229@gmail.com>
2016-11-03 16:43:53 -0500

    GA code

--- wp-content/themes/cerauno/header.php
+++ wp-content/themes/cerauno/header.php
@@ -14,6 +14,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85960212-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 <?php wp_head(); ?>
 </head>
